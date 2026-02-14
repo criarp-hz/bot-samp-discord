0abee125e5189c695ff48d8c12cf84c48449521f
criarp-hz <criarp21@gmail.com>
2026-02-14 02:57:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
 
+const app = express();
+app.use(express.json());
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+const TOKEN = process.env.TOKEN;
+
+client.once('ready', () => {
+    console.log(`Bot online: ${client.user.tag}`);
+});
+
+app.post('/chat', async (req, res) => {
+    try {
+        const { channelId, message } = req.body;
+        const channel = await client.channels.fetch(channelId);
+
+        if (channel) {
+            channel.send(message);
+        }
+
+        res.sendStatus(200);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+});
+
+app.listen(3000, () => {
+    console.log('API rodando na porta 3000');
+});
+
+client.login(TOKEN);
